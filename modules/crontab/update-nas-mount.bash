21dbca50be1215a98888e97dcae1d83f10b9f20f
Mauricio Jost <mauricio.jost@gmail.com>
2017-06-17 00:22:54 +0200

    Add logs to update-nas-mount.bash

--- modules/crontab/update-nas-mount.bash
+++ modules/crontab/update-nas-mount.bash
@@ -4,15 +4,16 @@ set -e
 set -x
 
 HOME_ROUTER_IP=10.0.0.1
+LOG=/tmp/update-nas-mount.bash.log
 
-ping -c 1 HOME_ROUTER_IP -W 1
+ping -c 1 $HOME_ROUTER_IP -W 1
 
 if [ "$?" == "0" ]
 then
-  echo "At home, mounting..."
-  mount /mnt/nas
+  echo "At home, mounting..." &>> $LOG
+  mount /mnt/nas &>> $LOG
 else
-  echo "Not at home, unmounting..."
-  sudo umount -f -l /mnt/nas
+  echo "Not at home, unmounting..." &>> $LOG
+  sudo umount -f -l /mnt/nas &>> $LOG
 fi
 
