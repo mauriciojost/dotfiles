f0abdef4039d46d5105aaa8dd1a0d510d656152d
Mauricio Jost <mauricio.jost@gmail.com>
2017-08-02 08:45:32 +0200

    Improve logs for update-nas-mount.bash

--- modules/crontab/update-nas-mount.bash
+++ modules/crontab/update-nas-mount.bash
@@ -6,17 +6,19 @@ set -x
 HOME_NAS_IP=10.0.0.8
 LOG=/tmp/update-nas-mount.bash.log
 
+echo "" >> $LOG
+echo "" >> $LOG
 date >> $LOG
 
 ping -c 1 $HOME_NAS_IP -W 1 &>> $LOG
 
 if [ "$?" == "0" ]
 then
-  echo "At home, mounting..." &>> $LOG
+  echo "### At home, mounting..." &>> $LOG
   mount /mnt/nas &>> $LOG
 else
-  echo "Not at home, unmounting..." &>> $LOG
+  echo "### Not at home, unmounting..." &>> $LOG
   sudo umount -f -l /mnt/nas &>> $LOG
 fi
 
-echo "Done." &>> $LOG
+echo "### Done." &>> $LOG
