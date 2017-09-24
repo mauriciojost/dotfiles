2f1ccab84c44a1f31ef24d1a3c213693ec5b5e30
Mauricio Jost <mauricio.jost@gmail.com>
2017-09-24 15:10:40 +0200

    Add mermaid example

--- modules/shell/aliases/image-editing.sh
+++ modules/shell/aliases/image-editing.sh
@@ -21,10 +21,18 @@ function qdiagram-scala() {
 	local executable="$DOTFILES/modules/scaladiagrams/scaladiagrams/scaladiagrams"
 	if [ -e "$executable" ]
 	then
-		echo "Running $executable $@"
-		"$executable" $@
+		"$executable" $@ | dot -Tpng > output.png
+		"$executable" $@ > output.dotfile
 	else
 		echo "Installation required."
 		cat "$DOTFILES/modules/scaladiagrams/README.md"
 	fi
 }
+
+function qdiagram-mermaid() {
+	echo "Packages here: https://unpkg.com/mermaid@7.1.0/dist/"
+	echo "Examples here: https://github.com/knsv/mermaid"
+	echo "Live editor: https://mermaidjs.github.io/mermaid-live-editor/"
+	gop $DOTFILES/modules/mermaid/index.html
+}
+
