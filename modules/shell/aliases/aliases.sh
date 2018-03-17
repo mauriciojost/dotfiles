b8135b883af1d1f47a783db5918ce6482f886a74
Mauricio Jost <mauricio.jost@gmail.com>
2018-03-17 08:01:39 +0100

    Add customisations of c and b functions

--- modules/shell/aliases/aliases.sh
+++ modules/shell/aliases/aliases.sh
@@ -1,3 +1,4 @@
+# Minor customisations to very used commands
 alias qdu='du -hs * | sort -h'
 alias gop='gnome-open'
 alias vim='vim -v'
@@ -6,27 +7,61 @@ alias htop="htop -u `whoami`"
 alias route='route -n'
 alias cal='cal -3'
 alias octave='octave --force-gui'
-alias b='cd ..'
-alias c='cd '
+alias grep='grep --color=always'
+
+# Go a level to the toop
+function b() {
+  cd ..
+  echo "### At: `pwd`"
+  tree -L 2 -d -x --filelimit 30
+  echo "..."
+  echo "### At: `pwd`"
+  echo ""
+  ls
+}
+
+# Enter a directory
+function c() {
+  local target="$1"
+
+  cd "$target"
+  echo "### At: `pwd`"
+  tree -L 2 -d -x --filelimit 30
+  echo "..."
+  echo "### At: `pwd`"
+  echo ""
+  ls
+}
+
 alias r='readlink -e '
+
+# Save and go to last saved directory
 alias sd='pwd > ~/.last-dir'
 alias gd='cd $(cat ~/.last-dir)'
+
+# Show tree in a fancy way
 alias tree='tree -s -h -f --du'
-alias tree-nohuman='tree -s -f --du'
+
+# Open a file
 alias gopen='thunar'
+
+# Open an editor
 alias gedit='mousepad'
 alias notepad='gedit'
 
+# Show our documents
 function qhelp() {
     ranger $DOTFILES/docs/
 }
 
+# Search a file in the current directory or sub directories
 function qf(){
     find | grep $1
 }
 
+# Edit an alias that matches a given pattern
 function qalias-edit() {
-	export ALIAS=$1
+	local ALIAS=$1
 	echo "Alias: $ALIAS"
 	FILES="`grep $ALIAS -l -R $DOTFILES/modules/shell/aliases/`"
 	echo "Found: "
