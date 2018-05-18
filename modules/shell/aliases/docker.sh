22fe8a41ab15da163d11f3785fe376dc62a4bcc1
Mauricio Jost <mauricio.jost@gmail.com>
2018-05-18 06:14:55 +0200

    Docker aliases

--- modules/shell/aliases/docker.sh
+++ modules/shell/aliases/docker.sh
@@ -16,6 +16,15 @@ alias qdock-images="sudo docker images"
 # Copy files to container
 alias qdock-scp="sudo docker cp"
 
+alias qdock-free-disk-space="sudo docker system prune"
+
+
+function qdock-create-container-from-image-X-and-start-it() {
+  sudo docker images
+  local cntr=`sudo docker create "$1"`
+  sudo docker start "$cntr"
+}
+
 function qdock-ip() {
   sudo docker inspect $1 | grep IPAddress
 }
