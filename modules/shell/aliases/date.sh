2ac03260e5ef84239de1ec4782f9487f0c414123
Mauricio Jost <mauricio.jost@gmail.com>
2018-09-13 08:13:00 +0200

    Add timestamp aliases

--- modules/shell/aliases/date.sh
+++ modules/shell/aliases/date.sh
@@ -1,1 +1,6 @@
 alias date='date "+%Y-%m-%d %H:%M"'
+
+alias timestamp_sec="python -c 'import time; print int(time.time())'"
+
+alias timestamp_ms="python -c 'import time; print int(time.time() * 1000)'"
+
