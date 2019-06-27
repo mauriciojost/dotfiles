502ae717eb1c06aef8412d0be0d264bc62ff3409
Mauricio Jost <mauricio.jost@gmail.com>
2019-06-27 18:27:21 +0200

    Improve qps

--- modules/shell/aliases/servers.sh
+++ modules/shell/aliases/servers.sh
@@ -9,23 +9,22 @@ function qx-display-lightdm-start() {
 function qps() {
     local tmp1=`mktemp`
     local tmp2=`mktemp`
-    local tmp3=`mktemp`
-    local tmp4=`mktemp`
     echo $tmp1
     echo $tmp2
     echo $tmp3
     echo $tmp4
     local cmd_length=80
     local filter=$1
-    ps -U $USER -k-%cpu -eo pid=PID,%cpu=CPU_PERC,thcount=NRO_THREADS,vsz=VIRTUAL_MEM_KB,rss=RESIDENT_MEM_KB,pri=PRIORITY,euser=USER,etime=TIME,cmd=LONG > $tmp1
+    ps -U $USER -k-%cpu -eo pid=PID,comm=BINARY,%cpu=CPU_PERC,thcount=NRO_THREADS,vsz=VIRTUAL_MEM_KB,rss=RESIDENT_MEM_KB,pri=PRIORITY,euser=USER,etime=TIME,cmd=LONG | sed -E 's/^[ ]+//g' | sed -E "s/[ ]+/\^/g"  | sed -E "s/\^/ /10g" > $tmp1
+
     if [ -z "$filter" ]
     then
-	cat $tmp1 >> $tmp4
+	cat $tmp1 >> $tmp2
     else
-	cat $tmp1 | grep $filter >> $tmp4
+	cat $tmp1 | grep $filter >> $tmp2
     fi
     # requires visidata installation
-    vd --csv-delimiter ' ' --default-width 10 --filetype csv --header 1 $tmp4
+    vd --csv-delimiter '^' --default-width 10 --filetype csv --header 1 $tmp2
 }
 
 function qvpn-over-ssh-using-server-X() {
