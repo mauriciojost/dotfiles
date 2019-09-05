bb5acd8c07a7f8ce405a0a821ed2ec378d3da966
Mauricio Jost <mauricio.jost@gmail.com>
2019-09-05 08:06:37 +0200

    Add history backup command

--- modules/shell/aliases/history.sh
+++ modules/shell/aliases/history.sh
@@ -10,6 +10,16 @@ function qhistory-list-consoles () {
 
 }
 
+function qhistory-backup() {
+    local d=`mktemp --directory`
+    cd $d
+    git clone $CUSTOM_HISTORY_FILE_BACKUP_GIT_REPO 
+    local f=$CUSTOM_HISTORY_FILE_BACKUP_GIT_REPO_DIR/$CUSTOM_HISTORY_FILENAME
+    cp $CUSTOM_HISTORY_FILE $f
+    cd $CUSTOM_HISTORY_FILE_BACKUP_GIT_REPO_DIR
+    git add -A && git commit -m "Backup"
+}
+
 alias qexecute-using-local-display="DISPLAY=:0 "
 
 function qh() {
