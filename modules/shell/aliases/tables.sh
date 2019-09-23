60f648b6f633a89c671afa5c3ba74c1810152fa6
Mauricio Jost <mauriciojost@gmail.com>
2019-09-23 08:38:36 +0200

    Add qsql-pretty

--- modules/shell/aliases/tables.sh
+++ modules/shell/aliases/tables.sh
@@ -2,3 +2,7 @@
 # Execute an sql query as follows: qsql "select * from table.csv"
 alias qsql="python2-q-text-as-data --tab-delimited-output --output-header --skip-header --delimiter='^'"
 
+function qsql-pretty(){
+  python2-q-text-as-data --tab-delimited-output --output-header --skip-header --delimiter='^' "$1" | vd
+}
+
