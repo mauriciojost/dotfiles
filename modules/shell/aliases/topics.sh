bbdfdfad121f083513d3d774fa1c9056956045e6
Mauricio Jost <mauriciojost@gmail.com>
2020-03-27 21:49:58 +0100

    Add topic aliases

--- modules/shell/aliases/topics.sh
+++ modules/shell/aliases/topics.sh
@@ -1,13 +1,31 @@
-function qtopic(){
-  local topic="$1"
-  local workspace=`bash -ic qworkspace-getname`
-  vim $HOME/topics/`date +"%Y-%m-%d"`."$topic".md
+
+function qtopic-list() {
+  ls -laht $HOME/topics/ | sort -r
+  echo "LATEST HERE ^^^ "
 }
 
-function qtopic-dir(){
+function qtopic-new-name() {
   local topic="$1"
-  local dir=$HOME/topics/`date +"%Y-%m-%d"`."$topic"
+  echo $HOME/topics/`date +"%Y-%m-%d"`."$topic"
+}
+
+function qtopic-new-file(){
+  local topic="$1"
+  local f=`qtopic-new-name $topic`.md
+  vim "$f"
+  echo "File: $f"
+}
+
+function qtopic-daily(){
+  qtopic-new-file "daily"
+}
+
+function qtopic-new-dir(){
+  local topic="$1"
+  local dir=`qtopic-new-name $topic`
   mkdir -p $dir
   ranger $dir
+  echo "Directory: $dir"
 }
 
+alias qtopic=qtopic-new-file
