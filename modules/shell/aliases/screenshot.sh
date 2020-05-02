f2970e45a63194f507d75ea43119ee80b7323677
Mauricio Jost <mauriciojost@gmail.com>
2020-05-02 16:26:04 +0200

    Improve screenshot function

--- modules/shell/aliases/screenshot.sh
+++ modules/shell/aliases/screenshot.sh
@@ -1,12 +1,13 @@
 
 # Screenshot
 function qscreenshot-region-file() {
+   local ld=`cat ~/.last-dir`
+   local new_name=$(zenity --entry --title="Name" --text="Filename" --entry-text="$RANDOM-$RANDOM")
+   local newoutputfile="$ld/$new_name.png"
+   echo "Will save to: $newoutputfile (change directory using sd command)"
    echo "Select region using mouse..."
-   gnome-screenshot -a 
-   export outputfile=$(ls -t "$HOME" | grep png | head -1 | xargs -I % readlink -e "$HOME/%")
-   export newoutputfile="/tmp/$RANDOM-$RANDOM-$RANDOM.png"
-   echo "Moving from $outputfile to $newoutputfile..."
-   mv "$outputfile" "$newoutputfile"
+   gnome-screenshot -a -f "$newoutputfile"
+   echo ""
    echo "Output file at: $newoutputfile"
 }
 
