f471fd168c70840ea86ea87cf90a897704f80f4b
Mauricio Jost <mauriciojost@gmail.com>
2020-05-24 22:47:12 +0200

    Some fzf aliases

--- modules/shell/aliases/fzf.sh
+++ modules/shell/aliases/fzf.sh
@@ -3,3 +3,11 @@ function cd_with_fzf() {
   cd "$(find . -type d -maxdepth 3 | fzf --preview="tree -L 2 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
 }
 
+function vim_with_fzf() {
+  vim $(fzf)
+}
+
+function history_with_fzf() {
+  cat $CUSTOM_HISTORY_FILE | fzf
+}
+
