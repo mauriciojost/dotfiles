d6a13b2b7086c014bbca2849c6601756e44b428a
Mauricio Jost <mauriciojost@gmail.com>
2020-05-25 00:36:50 +0200

    Integrate fzf for intelligent history

--- modules/shell/aliases/fzf.sh
+++ modules/shell/aliases/fzf.sh
@@ -1,13 +1,17 @@
-function cd_with_fzf() {
+function qfzf_cd_with() {
   cd $HOME 
   cd "$(find . -type d -maxdepth 3 | fzf --preview="tree -L 2 {}" --bind="space:toggle-preview" --preview-window=:hidden)"
 }
 
-function vim_with_fzf() {
-  vim $(fzf)
+function qfzf_vim_with() {
+  local f=$(fzf)
+  if [ -z "$f" ]
+  then
+    vim $(fzf)
+  fi
 }
 
-function history_with_fzf() {
+function qfzf_history_with() {
   cat $CUSTOM_HISTORY_FILE | fzf
 }
 
