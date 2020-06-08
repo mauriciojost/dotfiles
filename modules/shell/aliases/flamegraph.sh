955778e2dc5c3fa9166414518c920ef10c3e97a8
Mauricio Jost <mauriciojost@gmail.com>
2020-06-08 23:01:56 +0200

    Minor flamegraph log messages

--- modules/shell/aliases/flamegraph.sh
+++ modules/shell/aliases/flamegraph.sh
@@ -11,6 +11,8 @@ function qflamegraph-benchmark-pid-X-iters-Y-every-Z-secs() {
   i=0; while (( i++ < $iterations )); do jstack $pid >> $temp_stacks; sleep $period_secs; done
   cat $temp_stacks | $fg/stackcollapse-jstack.pl > $temp_acum
   $fg/flamegraph.pl $temp_acum > $temp_index_html
+  echo "Stacks in: $temp_stacks"
+  echo "Acums in: $temp_acum"
   echo "Report in: $temp_index_html"
 }
 
