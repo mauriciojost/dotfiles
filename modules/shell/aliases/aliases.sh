873091ad0e618c9557fd3e7ae5d8af09eac60d0c
Mauricio Jost <mauriciojost@gmail.com>
2020-06-25 16:19:02 +0200

    More aliases

--- modules/shell/aliases/aliases.sh
+++ modules/shell/aliases/aliases.sh
@@ -1,49 +1,56 @@
 # Minor customisations to very used commands
 alias qdu='du -hs * | sort -h'
 alias gop='exo-open'
-alias top='top -c'
 alias htop="htop -u `whoami`"
 alias route='route -n'
 alias cal='cal -3'
 alias octave='octave --force-gui'
 alias grep='grep --color=always'
 
-function e () {
-	qexplore
-}
-
 alias x=exit
-alias p='ping 8.8.8.8'
 
 # Put whatever is piped to qclip into the clipboard
 alias qclip='xclip -selection clipboard'
 
-
 alias r='readlink -e '
 
-# Show our documents
-function qhelp() {
-  ranger $DOTFILES/docs/
-}
-
-function qopen-by-content-cmd-X-dir-Y() {
+function qopen-by-content-cmd-X-dir-Y-what-Z() {
   local cmd="$1"
   local from="$2"
-  $cmd $(egrep . $from | fzf | awk -F: '{print $1}')
+  local what="$3"
+
+  if [ "$what" == "" ]
+  then
+    files="$(egrep . -r "$from" | fzf | awk -F: '{print $1}')"
+    if [ "$files" != "" ]
+    then
+      $cmd $files
+    fi
+  else
+    files="$(egrep $what -l -r $from)"
+    if [ "$files" != "" ]
+    then
+      echo "Found files: $files"
+      $cmd $files
+    else
+      echo "No files found."
+    fi
+  fi
+
 }
 
 # Edit an alias that matches a given pattern
 function qalias-edit() {
-  local alias="$1"
-  if [ "$alias" == "" ]
-  then
-    qopen-by-content-cmd-X-dir-Y vim "$DOTFILES/modules/shell/aliases/*" 
-  else
-    echo "Alias: $alias"
-    files="`grep $alias -l -R $DOTFILES/modules/shell/aliases/`"
-    echo "Found: "
-    echo $files
-    vim $files
-  fi
+  qopen-by-content-cmd-X-dir-Y-what-Z vim "$DOTFILES/modules/shell/aliases/" $@
 }
+
+alias qalias=qalias-edit
+
+# Show our documents
+function qdocs-edit() {
+  qopen-by-content-cmd-X-dir-Y-what-Z vim "$DOTFILES/docs/" $@
+}
+
+alias qdocs=qdocs-edit
+
 
