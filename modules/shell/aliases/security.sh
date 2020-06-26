278d0362b82728dc504b86a6b879db7f611809d2
Mauricio Jost <mauriciojost@gmail.com>
2020-06-27 00:10:27 +0200

    Add history cleanup scripts

---
+++ modules/shell/aliases/security.sh
@@ -0,0 +1,14 @@
+function qsecurity-cleanup-history-matching-single() {
+  local file="$1"
+  local match="$2"
+  cat $file | grep -v "$match" > $file.new
+  echo "Before: $(wc -l $file) lines"
+  echo "After : $(wc -l $file.new) lines"
+  echo "rm $file && mv $file.new $file # if fine"
+}
+
+function qsecurity-cleanup-history-matching() {
+  local match="$1"
+  qsecurity-cleanup-history-matching-single "$HOME/.history.log" "$match"
+  qsecurity-cleanup-history-matching-single "$HOME/.bash_history" "$match"
+}
