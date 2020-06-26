137ab3a233542912487a467a32906dec9b1a718f
Mauricio Jost <mauriciojost@gmail.com>
2020-06-26 23:40:31 +0200

    Remove docs.sh

--- modules/shell/aliases/docs.sh
+++
@@ -1,9 +0,0 @@
-# Edit an doc that matches a given pattern
-function qdoc-open-with-content-x() {
-  local content="$1"
-  echo "Content: $content"
-  local files="`grep $content -i -l -R $DOCS`"
-  echo "Found: "
-  echo $files
-  vim $files
-}
