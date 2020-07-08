6d6006ee68c514e7356f802fda9739232b8d6ffa
Mauricio Jost <mauriciojost@gmail.com>
2020-07-09 01:58:14 +0200

    Use ^ as delimiter

--- modules/shell/aliases/tables.sh
+++ modules/shell/aliases/tables.sh
@@ -1,6 +1,6 @@
 
 # Execute an sql query as follows: qsql "select * from table.csv"
-alias qsql="python2-q-text-as-data --tab-delimited-output --output-header --skip-header --delimiter='^'"
+alias qsql="python2-q-text-as-data --output-delimiter='^' --output-header --skip-header --delimiter='^'"
 
 function qsql-pretty(){
   python2-q-text-as-data --tab-delimited-output --output-header --skip-header --delimiter='^' "$1" | vd
