fe215f6cdff1f4975ebe02ab5fde2767e0a9e181
Mauricio Jost <mauriciojost@gmail.com>
2020-08-03 18:52:56 +0200

    Improve flamegraph script

--- modules/shell/aliases/flamegraph.sh
+++ modules/shell/aliases/flamegraph.sh
@@ -8,9 +8,16 @@ function qflamegraph-benchmark-pid-X-iters-Y-every-Z-secs() {
   temp_stacks=`mktemp`
   temp_acum=`mktemp`
   temp_index_html=`mktemp`.html
-  i=0; while (( i++ < $iterations )); do jstack $pid >> $temp_stacks; sleep $period_secs; done
+  i=0
+  while (( i++ < $iterations ))
+  do 
+    echo "$i/$iterations..."
+    jstack $pid >> $temp_stacks
+	sleep $period_secs
+  done
   cat $temp_stacks | $fg/stackcollapse-jstack.pl > $temp_acum
   $fg/flamegraph.pl $temp_acum > $temp_index_html
+  cat $temp_acum
   echo "Stacks in: $temp_stacks"
   echo "Acums in: $temp_acum"
   echo "Report in: $temp_index_html"
