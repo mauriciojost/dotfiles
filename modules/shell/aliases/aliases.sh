8ed81bd993046fc7feb49c9d1dd4266f1f378609
Mauricio Jost <mauriciojost@gmail.com>
2020-08-09 14:59:33 +0200

    Better display help

--- modules/shell/aliases/aliases.sh
+++ modules/shell/aliases/aliases.sh
@@ -14,18 +14,24 @@ function _xmllint() {
   xmllint --format "$f" | xmllint --xpath "$x" - | sed 's#name=#\n- #g' | sed 's# value=# ==> #g' | sed -e 's#&..;# #g' | sed 's#"##g' | sed -e 's#&quot;##g'
 }
 
-function qhelp() {
+function _xmllint_xfce() {
   local s="$DOTFILES/modules/xfce/xfce4.configlink/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
+  local x="$1"
+  _xmllint "$s" "$x" | grep -v 'override' | sed 's#  # #g'
+}
+
+
+function qhelp() {
   echo "### GENERAL SHORTCUTS"
   echo "ctrl + space                ulauncher"
   echo ""
   echo "### XFCE SHORTCUTS"
-  _xmllint "$s" '/channel[@name="xfce4-keyboard-shortcuts"]/property[@name="commands"]/property[@name="default"]/property[@type!="empty"]'
-  _xmllint "$s" "/channel[@name=\"xfce4-keyboard-shortcuts\"]/property[@name=\"commands\"]/property[@name=\"custom\"]/property/@*[name()='name' or name()='value']"
-  _xmllint "$s" "/channel[@name=\"xfce4-keyboard-shortcuts\"]/property[@name=\"xfwm4\"]/property[@name=\"custom\"]/property/@*[name()='name' or name()='value']"
+  _xmllint_xfce '/channel[@name="xfce4-keyboard-shortcuts"]/property[@name="commands"]/property[@name="default"]/property[@type!="empty"]'
+  _xmllint_xfce "/channel[@name=\"xfce4-keyboard-shortcuts\"]/property[@name=\"commands\"]/property[@name=\"custom\"]/property/@*[name()='name' or name()='value']"
+  _xmllint_xfce "/channel[@name=\"xfce4-keyboard-shortcuts\"]/property[@name=\"xfwm4\"]/property[@name=\"custom\"]/property/@*[name()='name' or name()='value']"
   echo ""
   echo "### SHELL BINDINGS"
-  _qset_bindings
+  _qset_bindings show
 
   echo ""
   echo "### CONFIGURATION APPS"
@@ -87,20 +93,28 @@ function _qbind() {
   local feature="$1"
   local shortcut="$2"
   local cmd="$3"
-  echo "Binding: $shortcut for $feature"
-  bind "$shortcut:$cmd"
+  local mode="$4" # show, set or showset
+  if [ "$mode" == "show" ] || [ "$mode" == "showset" ]
+  then
+    echo "- $shortcut ==> $feature" | sed 's#\\C-#Control #g' | sed 's#"##g'
+  fi
+  if [ "$mode" == "set" ] || [ "$mode" == "showset" ]
+  then
+    bind "$shortcut:$cmd"
+  fi
 }
 
 function _qset_bindings() {
+  local mode="$1"
    # The "$(cmd_with_stdout)\e\C-e\er" is a trick to write output into shell rather than executing it, so that it's stored in the history
-  _qbind "Quick CD (to chosen dir, CD to it)" '"\C-f"' '"qfzf_cd_with\n"' 
-  _qbind "Quick Find (by content in pwd, type vim cmd)" '"\C-a"' '"$(qfzf_here_by_content)\e\C-e\er"' 
-  _qbind "Quick Vim (by filename, type vim cmd)" '"\C-v"' ' "$(qfzf_vim_with)\e\C-e\er"'
-  _qbind "History++ (by date/dir/command in pwd, type)" '"\C-h"' '"$(qfzf_history_with)\e\C-e\er"'
-  _qbind "Chrome Hist. (by url, open)" '"\C-w"' '"$(qfzf_chrome_history_with)\e\C-e\er"'
-  _qbind "Topics (by content, copy to clipboard)" '"\C-t"' '"$(qfzf_topics)\e\C-e\er"'
+  _qbind "Quick CD (to chosen dir, CD to it)" '"\C-f"' '"qfzf_cd_with\n"' "$mode"
+  _qbind "Quick Find (by content in pwd, type vim cmd)" '"\C-a"' '"$(qfzf_here_by_content)\e\C-e\er"' "$mode"
+  _qbind "Quick Vim (by filename, type vim cmd)" '"\C-v"' ' "$(qfzf_vim_with)\e\C-e\er"' "$mode"
+  _qbind "History++ (by date/dir/command in pwd, type)" '"\C-h"' '"$(qfzf_history_with)\e\C-e\er"' "$mode"
+  _qbind "Chrome Hist. (by url, open)" '"\C-w"' '"$(qfzf_chrome_history_with)\e\C-e\er"' "$mode"
+  _qbind "Topics (by content, copy to clipboard)" '"\C-t"' '"$(qfzf_topics)\e\C-e\er"' "$mode"
   #C-j forbidden, causes strange behaviour
-  _qbind "Documents" '"\C-u"' '"qfzf_docs\n"'
+  _qbind "Documents" '"\C-u"' '"qfzf_docs\n"' "$mode"
 }
 
 
