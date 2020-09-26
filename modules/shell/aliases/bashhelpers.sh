39880d0bc5c03e5b02df923097e2e96597dcb7f0
Mauricio Jost <mauriciojost@gmail.com>
2020-09-26 05:43:02 +0200

    Integrate shellcheck (thanks Mathieu)

--- modules/shell/aliases/bashhelpers.sh
+++ modules/shell/aliases/bashhelpers.sh
@@ -1,14 +1,20 @@
 
 function qassertnotempty() {
-	if [ -z "$1" ]
-	then
-		echo "$2" 
-		return 1
-	fi
+  if [ -z "$1" ]
+  then
+    echo "$2" 
+    return 1
+  fi
 }
 
 function echoerr() {
-	echo "$@" | perl -ne 'print STDERR'
+  echo "$@" | perl -ne 'print STDERR'
 }
 
+
+function qshell-check() {
+  # apt-get install shellcheck
+  local script="$1"
+  shellcheck "$script"
+}
 
