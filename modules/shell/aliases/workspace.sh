0178456472a4a35ca0cd6f3a85038a279e56a325
Mauricio Jost <mauriciojost@gmail.com>
2020-10-17 15:35:36 +0200

    Normalized workspace names

--- modules/shell/aliases/workspace.sh
+++ modules/shell/aliases/workspace.sh
@@ -1,6 +1,12 @@
-#alias qworkspace-rename='$DOTFILES/modules/xfce/rename-xfce-workspace/rename-xfce-workspace/rename-xfce-workspace'
 function qworkspace-getname() {
-  wmctrl -d | grep '*' | sed 's/.* //g'
+  # current workspace index
+  curnt_ws_idx=$(wmctrl -d | grep '*' | cut -d " " -f1)
+  # Get the names of all the workspaces
+  ws_names=()
+  while read name; do
+    ws_names+=("$name")
+  done < <(xfconf-query -c xfwm4 -p /general/workspace_names | tail -n +3)
+  echo "${ws_names[$curnt_ws_idx]}"
 }
 
 function qworkspace-rename() {
@@ -14,8 +20,9 @@ function qworkspace-rename() {
     done < <(xfconf-query -c xfwm4 -p /general/workspace_names | tail -n +3)
 
     # Get new workspace name via zenity
-    new_name=$(zenity --entry --title="Rename workspace" \
+    new_name_raw=$(zenity --entry --title="Rename workspace" \
 	--text="Rename workspace $((curnt_ws_idx + 1))" --entry-text="${ws_names[$curnt_ws_idx]}")
+    new_name="$(echo ${new_name_raw,,} | sed 's# ##g')"
 
     # Update the names of all the workspaces (they could have changed in the meanwhile)
     ws_names=()
