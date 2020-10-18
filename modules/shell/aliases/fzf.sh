3c42d8098144ed2042dd13d744d07a314a4bad43
Mauricio Jost <mauriciojost@gmail.com>
2020-10-18 02:05:27 +0200

    Share find arguemtns

--- modules/shell/aliases/fzf.sh
+++ modules/shell/aliases/fzf.sh
@@ -4,6 +4,7 @@ export _FILE_CONTENT_MAX_FZF=300
 
 export _FZF_HIGHLIGHT="--color='bg:#4B4B4B,bg+:#3F3F3F,info:#BDBB72,border:#6B6B6B,spinner:#98BC99' --color='hl:#44CC44,fg:#D9D9D9,header:#719872,fg+:#D9D9D9' --color='pointer:#E12672,marker:#E17899,header:#98BEDE,hl+:#22FF22'"
 alias _fzf="fzf $_FZF_HIGHLIGHT --border=sharp"
+find_args='! -name *.class ! -path *.git*'
 function _typical_dirs() {
   local pref="$1"
   local output="$pref $TOPICS $pref $DOTFILES/docs"
@@ -17,13 +18,13 @@ function _typical_dirs() {
 }
 
 function qfzf_cd_with() {
-  cd "$(find $pwd -maxdepth 3 -type d 2>/dev/null | _fzf --header="CD TO..." --preview="tree -L 2 {}" --bind="left:toggle-preview")"
+  cd "$(find $pwd -maxdepth 3 -type d $find_args 2>/dev/null | _fzf --header="CD TO..." --preview="tree -L 2 {}" --bind="left:toggle-preview")"
 }
 
 
 # Search on typical dirs by filename (not by content) and stdout vim command on choice (search is on typical dirs)
 function qfzf_vim_with() {
-  echo vim "$(find $(_typical_dirs) -type f ! -name "*.class" ! -path "*/.git/*" 2>/dev/null | _fzf --header="EDIT..." --preview="head -100 {}" --bind="left:toggle-preview")"
+  echo vim "$(find $(_typical_dirs) -type f $find_args 2>/dev/null | _fzf --header="EDIT..." --preview="head -100 {}" --bind="left:toggle-preview")"
 }
 
 function qfzf_history_with() {
@@ -41,7 +42,7 @@ function qfzf_history_light_local_with() {
 
 function qfzf_chrome_history_with() {
   local backup="/tmp/chrome-history-fzf-search"
-  find $HOME/.config/google-chrome/ -type f -name History > /tmp/chrome-history-files.list
+  find $HOME/.config/google-chrome/ $find_args -type f -name History > /tmp/chrome-history-files.list
   echo "" > /tmp/chrome-history-urls.list
   while IFS= read -r line
   do
@@ -57,12 +58,12 @@ function qfzf_chrome_history_with() {
 function qfzf_file_path_egrepargs_X_by_filenamecontent() {
   local from="$1"
   local header="$2"
-  f="$(find $from -type f | while IFS= read -r f ; do echo "$f: $(head -$_FILE_CONTENT_MAX_FZF "$f" | tr -d '\n' | tr -d '\0')"; done | _fzf --header="$header" | awk -F: '{print $1}')"
+  f="$(find $from -type f $find_args | while IFS= read -r f ; do echo "$f: $(head -$_FILE_CONTENT_MAX_FZF "$f" | tr -d '\n' | tr -d '\0')"; done | _fzf --header="$header" | awk -F: '{print $1}')"
   if [ "$f" != "" ]
   then
     echo "vim $f"
   else
-    echo "# Nothing found"
+    echo ""
   fi
 }
 
