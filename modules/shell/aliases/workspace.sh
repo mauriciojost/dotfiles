d17839a7469c161e14d04b35cde8bf008ae7a102
Mauricio Jost <mauriciojost@gmail.com>
2020-10-23 08:53:56 +0200

    Some workspace stuff

--- modules/shell/aliases/workspace.sh
+++ modules/shell/aliases/workspace.sh
@@ -1,3 +1,11 @@
+function qworkspace-list() {
+  # Get the names of all the workspaces
+  ws_names=()
+  while read name; do
+    echo "$name"
+  done < <(xfconf-query -c xfwm4 -p /general/workspace_names | tail -n +3)
+}
+
 function qworkspace-getname() {
   # current workspace index
   curnt_ws_idx=$(wmctrl -d | grep '*' | cut -d " " -f1)
@@ -74,13 +82,21 @@ function qworkspace-goto-window-id() {
   fi
 }
 
+function qworkspace-list-apps-csv() {
+  echo "wid^id^name"
+  wmctrl -l | awk -F' ' '{i=$1; w=$2; h=$3; $1=$2=$3=""; print w "^" i "^" $0}'| sed -E 's/\^ +/\^/g' | sort -n
+}
+
+function qworkspace-list-csv() {
+  echo "id^name"
+  wmctrl -d | awk -F' ' '{i=$1; $1=$2=$3=$4=$5=$6=$7=$8=$9=""; print i "^" $0}' | sed -E 's/\^ +/\^/g' | sort -n
+}
+
 function qworkspace-list-windows() {
   local d="$(mktemp -d)"
   cd "$d"
-  echo "wid^id^name" > apps
-  wmctrl -l | awk -F' ' '{i=$1; w=$2; h=$3; $1=$2=$3=""; print w "^" i "^" $0}'| sed -E 's/\^ +/\^/g' | sort -n >> apps
-  echo "id^name" > works
-  wmctrl -d | awk -F' ' '{i=$1; $1=$2=$3=$4=$5=$6=$7=$8=$9=""; print i "^" $0}' | sed -E 's/\^ +/\^/g' | sort -n >> works
+  qworkspace-list-apps-csv > apps
+  qworkspace-list-csv > works
   python2-q-text-as-data --output-delimiter='^' -b --output-header --skip-header --delimiter='^' "select w.name as workspace,a.name as application, a.id as application_id from apps as a join works as w where a.wid = w.id"
   rm apps works
   cd -
@@ -105,5 +121,33 @@ function go() {
   else
     qworkspace-goto-window "$name"
   fi
+}
+
+function qworkspace-tidy() {
+  qworkspace-list-apps-csv > apps
+  qworkspace-list-csv > works
+  cat works | while read ws
+  do
+    if [ "$ws" != "id^name" ] # skip header
+    then
+      ws_id="$(echo "$ws" | awk -F^ '{print $1}')"
+      ws_name="$(echo "$ws" | awk -F^ '{print $2}')"
+      if [ "$ws_name" != "-" ]
+      then
+	cat apps | while read app
+	do
+	  if [ "$app" != "wid^id^name" ] # skip header
+	  then
+	    app_id="$(echo "$app" | awk -F^ '{print $2}')"
+	    app_name="$(echo "$app" | awk -F^ '{print $3}')"
+	    if [[ "${app_name,,}" == *"${ws_name,,}"* ]]; then
+	      echo "$app_name($app_id) -> $ws_name($ws_id)"
+	      wmctrl -i -r $app_id -t $ws_id
+	    fi
+	  fi
+	done
+      fi
+    fi
+  done
 }
 
