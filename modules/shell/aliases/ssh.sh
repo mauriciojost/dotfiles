6f869291956dd56f67ff981e20e23739d3c591f3
Mauricio Jost <mauriciojost@gmail.com>
2020-10-25 14:58:06 +0100

    Add ssh tunnel alias

--- modules/shell/aliases/ssh.sh
+++ modules/shell/aliases/ssh.sh
@@ -1,1 +1,8 @@
 alias qssh-retrieve-public-key-from-private-key='ssh-keygen -y -f ~/.ssh/id_rsa'
+
+function qssh-tunnel-dst-hostX-portY-locportZ() {
+  local user_server="$1"
+  local dest_port="$2"
+  local source_port="$3"
+  ssh -N -L "$source_port":127.0.0.1:"$dest_port" "$user_server"
+}
