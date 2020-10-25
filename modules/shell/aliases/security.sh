7a652b7ea56257dae029e10ecc2672bc1b326938
Mauricio Jost <mauriciojost@gmail.com>
2020-10-25 22:05:43 +0100

    Add some security scripts

--- modules/shell/aliases/security.sh
+++ modules/shell/aliases/security.sh
@@ -13,3 +13,37 @@ function qsecurity-cleanup-history-matching() {
   qsecurity-cleanup-history-matching-single "$CUSTOM_HISTORY_FILE" "$match"
   qsecurity-cleanup-history-matching-single "$HOME/.bash_history" "$match"
 }
+
+function qsecurity-install-certificate-linux() {
+  local servername="$1";
+  local serverport="$2";
+  local pem_file=$servername.$serverport.cert.pem.crt
+  openssl s_client -showcerts -connect $servername:$serverport < /dev/null 2> /dev/null | openssl x509 -outform PEM > $pem_file
+  sudo mkdir -p /usr/local/share/ca-certificates/extra
+  sudo cp $pem_file /usr/local/share/ca-certificates/extra/
+  sudo update-ca-certificates
+}
+
+function qsecurity-install-all-certificats-in-linux-for-chrome() {
+  # from https://thomas-leister.de/en/how-to-import-ca-root-certificate/
+  # Requirement: apt install libnss3-tools
+  for certfile in $(find /usr/local/share/ca-certificates/extra/ -type f)
+  do
+    echo "Inserting $certfile ..."
+	certname="$(basename $certfile)"
+	### For cert8 (legacy - DBM)
+	for certDB in $(find ~/ -name "cert8.db")
+	do
+	  certdir=$(dirname ${certDB});
+	  certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
+      echo "Inserting $certfile into $certdir..."
+	done
+	### For cert9 (SQL)
+	for certDB in $(find ~/ -name "cert9.db")
+	do
+	  certdir=$(dirname ${certDB});
+	  certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
+      echo "Inserting $certfile into $certdir..."
+	done
+  done
+}
