8e2b2bd26cdf3be9b7c7672d15869f9dbd36f5bd
Mauricio Jost <mauriciojost@gmail.com>
2020-10-28 12:25:26 +0100

    Add xfconf reset too

--- modules/shell/aliases/xfconf.sh
+++ modules/shell/aliases/xfconf.sh
@@ -5,9 +5,9 @@ function qxfconf-list-channels() {
 
 function qxfconf-dump-props-for-channel-x() {
   local channels="$@"
-  for c in $channels 
+  for channel in $channels 
   do 
-    xfconf-query -lv -c "${c}" | awk -F' ' '{a=$1;$1="";print a"^"$0}' | awk -F'^' '{print $1"^"$2}' | sed 's/\^ /^/g' | sort
+    xfconf-query -lv -c "$channel" | awk -F' ' '{a=$1;$1="";print a"^"$0}' | awk -F'^' '{print $1"^"$2}' | sed 's/\^ /^/g' | sort
   done
 }
 
@@ -26,12 +26,36 @@ function qxfconf-load-props-for-channel-x() {
   do
     prop="$(echo $line | awk -F^ '{print $1}' | sed 's/ *$//g' | sed 's/^ *//g' )"
     valu="$(echo $line | awk -F^ '{print $2}' | sed 's/ *$//g' | sed 's/^ *//g' )"
-    xfconf-query -v -c "${c}" -p "$prop" -s "$valu" --create
+    patString='[a-zA-Z]+'
+    patNumber='[0-9]+'
+    if [ valu == "true" ] || [ "$valu" == "false" ]
+    then
+      type=bool
+    elif [[ $valu =~ $patString  ]]
+    then
+      type=string
+    elif [[ $valu =~ $patNumber  ]]
+    then
+      type=int
+    else
+      type=unknown
+    fi
+    echo "Loading channel='$channel' property='$prop' value='$valu' (inferred type='$type')"
+    xfconf-query -v -c "$channel" -p "$prop" -s "$valu" --create -t $type
+  done
+}
+
+function qxfconf-reset-props-for-channel-x() {
+  local channel="$1"
+  qxfconf-dump-props-for-channel-x "$c" | awk -F^ '{print $1}' | while read propname
+  do
+    xfconf-query -v -c "$channel" -p "$propname" --reset
   done
 }
 
 function qxfconf-load() {
-  for c in $(qxfconf-list-channels)
+  local channels="${1:-$(qxfconf-list-channels)}"
+  for c in $channels
   do 
     f="$DOTFILES/modules/xfconf/xfconf.$c.conf"
     if [ -e "$f" ]
@@ -41,5 +65,13 @@ function qxfconf-load() {
     else
       echo "Skipping channel: $c (no file present) ..."
     fi
+  done
+}
+
+function qxfconf-reset() {
+  local channels="${1:-$(qxfconf-list-channels)}"
+  for c in $channels
+  do 
+    qxfconf-reset-props-for-channel-x "$c"
   done
 }
