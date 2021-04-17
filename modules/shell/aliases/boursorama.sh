9ee263f7052eb8fd2bf9851c281cc458084e9477
Mauricio Jost <mauriciojost@gmail.com>
2021-04-17 15:46:13 +0200

    Better boursorama

--- modules/shell/aliases/boursorama.sh
+++ modules/shell/aliases/boursorama.sh
@@ -5,6 +5,10 @@ function _qboursorama-match() {
     *categoryParent*)                   echo "$line;customCategory;customSubcategory;customDescription" ;; # header
 
     # weekly shopping
+    *netto*)                            echo "$line;shopping;food;regular" ;;
+    *u*express*)                        echo "$line;shopping;food;regular" ;;
+    *monoprix*)                         echo "$line;shopping;food;regular" ;;
+    *naturalia*)                        echo "$line;shopping;food;regular" ;;
     *carrefour*)                        echo "$line;shopping;food;regular" ;;
     *bioman*)                           echo "$line;shopping;food;bio" ;;
     *francis*viande*)                   echo "$line;shopping;food" ;;
@@ -22,6 +26,7 @@ function _qboursorama-match() {
     *sc*armand*piet*)                   echo "$line;shopping;boulangerie;boulangerie" ;;
     *la*boulangerie*)                   echo "$line;shopping;boulangerie;boulangerie" ;;
     *paul*republique*)                  echo "$line;shopping;boulangerie;panaderia en av de la republique" ;;
+    *l*artiste*pain*)                   echo "$line;shopping;boulangerie;panaderia" ;;
     *cep*boulanger*)                    echo "$line;shopping;boulangerie;panaderia" ;;
     *hoggar*viandes*)                   echo "$line;shopping;food;meat" ;;
     *lart*et*le*pain*)                  echo "$line;shopping;boulangerie;panaderia" ;;
@@ -38,6 +43,7 @@ function _qboursorama-match() {
     *vente*privee*)                     echo "$line;baby;clothes;" ;;
     *pinocchio*)                        echo "$line;baby;games;" ;;
     *la*grande*recr*)                   echo "$line;baby;games;" ;;
+    *la*recree*)                        echo "$line;baby;games;" ;;
     *barbe*de*papa*)                    echo "$line;baby;haircut;" ;;
     *oeuvre*des*creches*de*nice*)       echo "$line;baby;creche;" ;;
     *du*pareil*au*me*)                  echo "$line;baby;clothes;" ;;
@@ -49,6 +55,7 @@ function _qboursorama-match() {
     *station*avia*)                     echo "$line;leisure;transport;fuel avia" ;;
     *paybyphone*nic*)                   echo "$line;service;parking;auto parking mensual" ;;
     *agip*)                             echo "$line;service;transport;fuel agip" ;;
+    *paypal*)                           echo "$line;service;paypal;netflix-spotify-etc" ;;
 
     # trip
     *volotea*)                          echo "$line;leisure;trip;plane" ;;
@@ -64,8 +71,10 @@ function _qboursorama-match() {
     *gras*savoye*)                      echo "$line;health;mutuel;gras savoye" ;;
 
     # taxes
-    *direction*generale*des*finance*)   echo "$line;tax;unknown" ;;
+    *direction*generale*des*finance*)   echo "$line;tax;unknown;" ;;
     *caf*alpes*)                        echo "$line;tax;caf;" ;;
+    *contravention*)                    echo "$line;tax;contravention;multas" ;;
+    *forf*p*stat*web*)                  echo "$line;tax;contravention;multas" ;;
 
     # insurance
     *cpam*061*nice*)                    echo "$line;insurance;securite-social;securite-social" ;;
@@ -80,11 +89,15 @@ function _qboursorama-match() {
     *edf*)                              echo "$line;service;edf;electricity" ;;
     *google*)                           echo "$line;service;googledrive;nas" ;;
     *vir*p1*internet*tv*)               echo "$line;service;tvinternet;" ;;
+    *la*poste*)                         echo "$line;service;laposte;" ;;
+    *orange*)                           echo "$line;service;internet;orange" ;;
 
     # vetement
     *hetm234*)                          echo "$line;basic;clothes;" ;;
+    *vp.com*)                           echo "$line;basic;clothes;" ;;
 
     # marta unknown
+    *sumup*saby*)                       echo "$line;leisure;unclear;martatoexplain" ;;
     *selvi*didier*)                     echo "$line;leisure;unclear;martatoexplain" ;;
     *jm*et*mf*)                         echo "$line;leisure;unclear;martatoexplain" ;;
     *zara*home*)                        echo "$line;leisure;unclear;martatoexplain" ;;
@@ -107,6 +120,11 @@ function _qboursorama-match() {
     *sc*goodfood*)                      echo "$line;leisure;unclear;martatoexplain" ;;
     *pastry*plaisir*)                   echo "$line;leisure;unclear;martatoexplain" ;;
     *pianelli*freres*)                  echo "$line;leisure;unclear;martatoexplain-crepes" ;;
+    *arpisante*)                        echo "$line;leisure;unclear;martatoexplain-parfums-pharmacie" ;;
+    *peisino*)                          echo "$line;leisure;unclear;martatoexplain-pharmacie" ;;
+    *constans*et*fi*)                   echo "$line;leisure;unclear;martatoexplain-flowersrosesantibes" ;;
+    *rodi*fleurs*)                      echo "$line;leisure;unclear;martatoexplain-flowers" ;;
+    *sc*a*la*mic*)                      echo "$line;leisure;unclear;martatoexplain-smth" ;;
     *armee*alpes*)                      echo "$line;leisure;unclear;mauritoexplain" ;;
 
     # feeding operations
@@ -130,11 +148,13 @@ function _qboursorama-match() {
     *canet*maccaran*)                   echo "$line;leisure;boulangerie;boulangerie chic" ;;
     *lac*barla*)                        echo "$line;leisure;boulangerie;boulangerie chic" ;;
     *lekobe*)                           echo "$line;leisure;luxury;sushi" ;;
+    *vialatina*)                        echo "$line;leisure;boulangerie;cafe" ;;
     *patisserie*)                       echo "$line;leisure;unclear;patiseria" ;;
     *burlet*location*de*skis*)          echo "$line;leisure;ski;ski" ;;
     *tiger*)                            echo "$line;leisure;unclear;tiger" ;;
     *delices*de*la*republique*)         echo "$line;leisure;unclear;patiseria" ;;
     *pane*e*olio*)                      echo "$line;leisure;restaurant;coulee verte" ;;
+    *sushi*)                            echo "$line;leisure;restaurant;sushi" ;;
     # food delivery
     *uber*)                             echo "$line;leisure;delivery;food" ;;
     *deliveroo*)                        echo "$line;leisure;delivery;food" ;;
@@ -146,36 +166,52 @@ function _qboursorama-match() {
   esac
 }
 function qboursorama-analyse() {
-  local base="$HOME/Downloads"
+  local iso_month=${1:-20}
   local base="$HOME/Downloads"
   #local view_tool="fzf --no-sort"
   local view_tool="vd --csv-delimiter ';' --default-width 30 --filetype csv --header 1"
   rm -f /tmp/bourso.*
-  echo "Remember to:"
-  echo "- go to Evolution de mon solde "
-  echo "- select the card you are interested in "
-  echo "- select the time period you are interested in (remember that limits are inclusive, i.e. for a full month 2020-01-01 to 2020-01-31)"
-  echo "- then click on Exporter au format csv (file must land in $HOME/Downloads/export...csv)"
-  echo "Helpers (visidata):"
-  echo "- shift + C to go to columns view, e to edit, q to return"
-  echo "- set column to ~ (string), # (int), % (float), $ (currency), or @ (date)"
-  echo "- [ and ] sorts"
-  echo "- | + text + enter selects lines and \" creates a temp table to see only those"
-  echo "- +agg (like +sum) on a given column sets such aggregator, then select keys with ! and then shift+w to make pivot, q to exit it"
-  echo "- ! sets a column as a key"
-  echo "- shift+w to pivot on given table"
-  echo "- h / l to move column to right/left "
-  echo "- - to hide column"
-  echo "- gv to show all columns"
-  echo "- q to exit"
-  echo ""
-  echo "Press ENTER once the file has been downloaded..."
+  cat <<EOL >> /tmp/bourso.help
+
+
+VISIDATA helpers:
+- general
+    - change type of column:             ~ (str), # (int), % (float), $ (currency), or @ (date)
+    - sort by column:                   [ and ]
+    - move column to right/left:        h and l
+    - hide column:                      -
+    - full column width:                _
+    - show all columns:                 gv
+    - exit:                             q
+- filter rows
+    - | + text + enter selects lines and \" creates a temp table to see only those
+- see only certain columns (advanced)
+    1. go to columns view:    shift + C 
+    2. edit columns view:     e
+    3. exit:                  q
+- create aggregate views
+    1. set columns as key (for aggregation): !
+    2. set aggregator on a given column:     +agg (like +sum) 
+    3. create pivot:                         shift+w
+    4. exit:                                 q
+EOL
+
+  cat <<EOL
+USE:
+- LOG IN into boursorama, account, Graphique, Evolution de mon solde 
+- select the cards you are interested in 
+- select the time period you are interested in (limits are INCLUSIVE, i.e. for a full month 2020-01-01 to 2020-01-31)
+- then click on Exporter au format csv (file must land in $base/export...csv)
+
+Press ENTER once the file has been downloaded in $base...
+EOL
+
   read -i xx
   local input="$(ls -t $base/export*.csv | head -1)"
   if [ -e "$input" ]
   then
-	iconv -f ISO-8859-1 -t UTF-8 "$input" -o /tmp/bourso.csv.tmp
-	cat /tmp/bourso.csv.tmp | sed -E 's/(.*)([0-9]) ([0-9])(.*)/\1\2\3\4/g' > /tmp/bourso.csv.tmp2 # values greater than 1k are represented wrongly (for instance 1300 would be represented as 1 300 by boursorama)
+	iconv -f ISO-8859-1 -t UTF-8 "$input" -o /tmp/bourso.csv.tmp1
+	cat /tmp/bourso.csv.tmp1 | sed -E 's/(.*)([0-9]) ([0-9])(.*)/\1\2\3\4/g' > /tmp/bourso.csv.tmp2 # values greater than 1k are represented wrongly (for instance 1300 would be represented as 1 300 by boursorama)
         
 	while IFS= read -r line
 	do
@@ -183,9 +219,24 @@ function qboursorama-analyse() {
 	done < /tmp/bourso.csv.tmp2
 
 
-	python2-q-text-as-data --output-header --skip-header --delimiter=';' --output-delimiter=';' 'select *, substr(dateOp,7)|| "-" ||substr(dateOp,4,2)|| "-" ||substr(dateOp,1,2) as dateOpIso from /tmp/bourso.csv.tmp3' > /tmp/bourso.csv
+	python2-q-text-as-data --output-header --skip-header --delimiter=';' --output-delimiter=';' 'select *, substr(dateOp,7)|| "-" ||substr(dateOp,4,2)|| "-" ||substr(dateOp,1,2) as dateOpIso from /tmp/bourso.csv.tmp3 where dateOpIso like "'$iso_month'%"' > /tmp/bourso.csv
 
-	screen -AdmS myshell -t balance bash -c "python2-q-text-as-data -b --output-header --skip-header --delimiter=';' 'select sum(amount) as balance, min(dateOpIso) as from_date_inclusive, max(dateOpIso) as until_date_inclusive from /tmp/bourso.csv' | $view_tool"
+cat <<EOL >> /tmp/bourso.csv.report
+REPORT
+
+1. Lines check:
+- step 1: $(cat /tmp/bourso.csv.tmp1 | wc -l)
+- step 2: $(cat /tmp/bourso.csv.tmp2 | wc -l)
+- step 3: $(cat /tmp/bourso.csv.tmp3 | wc -l)
+- step 4: $(cat /tmp/bourso.csv      | wc -l)
+
+2. Balance:
+$(python2-q-text-as-data -T -b --output-header --skip-header --delimiter=';' 'select sum(amount) as balance, min(dateOpIso) as from_date_inclusive, max(dateOpIso) as until_date_inclusive from /tmp/bourso.csv')
+EOL
+
+	screen -AdmS myshell -t help bash -c "less /tmp/bourso.help"
+
+	screen -S myshell -X screen -t report bash -c "cat /tmp/bourso.csv.report | less"
 
 	screen -S myshell -X screen -t summary bash -c "python2-q-text-as-data -b --output-header --skip-header --delimiter=';' 'select substr(r.dateOpIso, 1,7) as month, r.customCategory as category, sum(r.amount) as consumed from /tmp/bourso.csv as r group by month, r.customCategory' | $view_tool"
 
