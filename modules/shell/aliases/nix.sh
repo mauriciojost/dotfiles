c50f010d5d93c2cf5240b0aed59b23cafcb161e7
Mauricio Jost <mauriciojost@gmail.com>
2021-05-27 19:05:33 +0200

    Add nix aliases

--- modules/shell/aliases/nix.sh
+++ modules/shell/aliases/nix.sh
@@ -1,6 +1,13 @@
 alias qnix-list='nix-env -q'
 alias qnix-install='nix-env -i'
 
+# from https://nixos.wiki/wiki/Nix
+alias qnix-package-search='nix search'
+alias qnix-package-install='nix-env -iA'
+alias qnix-package-list-installed='nix-env -q'
+alias qnix-package-uninstall='nix-env -e'
+alias qnix-package-upgrade-all='nix-env -u'
+
 function qnix-explore-db() {
   nix-env -iA sqlite -f '<nixpkgs>'
   sqlite3 /nix/var/nix/db/db.sqlite
