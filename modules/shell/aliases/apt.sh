8c4b8935c23067b38f3e2a31cb2449dfe029b358
Mauricio Jost <mauriciojost@gmail.com>
2022-06-30 12:44:37 +0200

    Function qdist-upgrade to save previous + after packages

--- modules/shell/aliases/apt.sh
+++ modules/shell/aliases/apt.sh
@@ -9,3 +9,19 @@ alias qpackage-what-package-provides-the-file='apt-file search'
 
 alias qyum-provides='apt-file search'
 
+
+function qdist-upgrade() {
+  local tmpd=~/.logs
+  mkdir -p $tmpd 
+  local fn=$tmpd/dist-upgrade.$(date '+%Y-%m-%d-%H-%M-%S').log
+  date &>> $fn
+  echo "installed before" &>> $fn
+  apt list --installed &>> $fn
+  echo "upgradable before" &>> $fn
+  apt list --upgradeable &>> $fn
+  sudo apt-get dist-upgrade
+  echo "installed after" &>> $fn
+  apt list --installed &>> $fn
+  echo "upgradable after" &>> $fn
+  apt list --upgradeable &>> $fn
+}
